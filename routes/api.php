cab6b8f2220f889758430fac6d57acad24efbed3
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-10-29 18:12:03 +0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTAuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'auth'], function ($router) {
+    Route::post('register', [JWTAuthController::class, 'register']);
+    Route::post('login', [JWTAuthController::class, 'login']);
+});
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('me', [JWTAuthController::class, 'getUser']);
+    Route::post('refresh', [JWTAuthController::class, 'refresh']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
